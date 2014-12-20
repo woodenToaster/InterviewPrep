72ac430d46e81f7c47f6899d004508caf6c1ffd5
woodenToaster <litmusik@yahoo.com>
2014-12-20 14:50:32 -0600

    Reverse a C-string

---
+++ reverse.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+char* reverse(char *str);
+
+int main(int argc, char* argv[]) {
+
+  char *str = argv[1];
+
+  char *rStr = reverse(str);
+  printf("%s\n", rStr);
+  return 0;
+}
+
+char* reverse(char *str) {
+  if(str == NULL) {
+    printf("Error\n");
+    exit(-1);
+  }
+  char *rStr;
+  int end = strlen(str) - 1;
+  char *begin = str;
+
+  rStr = &str[end];
+
+  while(str != rStr && str < rStr) {
+    //Swap
+    char tmp = *str;
+    *str = *rStr;
+    *rStr = tmp;
+    str++;
+    rStr--;
+  }
+  return begin;
+}
